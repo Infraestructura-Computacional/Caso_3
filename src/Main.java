647efbdab7a2b9d14be5ad6a53d1d9a305e2aab7
Juanes Alvarez <je.alvarezg1@uniandes.edu.co>
2024-11-03 13:18:16 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
      public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+          Main mainInstance = new Main(); // Crear una instancia de Main
           
-     }
+          System.out.println("1) Generar pareja de llaves");
+          System.out.println("2) Ejecutar");
+          int opcion = scanner.nextInt();
+          
+          if (opcion == 1) {
+              mainInstance.generarLlaves(); // Llamar a través de la instancia
+          } else if (opcion == 2) {
+              mainInstance.ejecutar(); // Llamar a través de la instancia
+          } else {
+              System.out.println("Opción inválida. Por favor, digite 1 o 2.");
+          }
+  
+          scanner.close();
+      }
+
+    //TODO
+    public void generarLlaves(){
+
+    }
+
+    //TODO
+    public void ejecutar(){
+
+    }
 }
